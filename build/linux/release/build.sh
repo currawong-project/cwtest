78732f60e84a8e88aef19e9c0a48721e4fa4d7ff
kevin <kpl@larke.org>
2014-01-26 16:12:22 -0500

    Makefile.am,debug/build.sh,release/build.sh : Added note on using gprof

--- build/linux/release/build.sh
+++ build/linux/release/build.sh
@@ -7,6 +7,11 @@ autoreconf --force --install
 
 cd ${curdir}
 
+# To Profile w/ gprof:
+# 1) Modify configure: ./configure --disable-shared CFLAGS="-pg"
+# 2) Run the program. ./foo
+# 3) Run gprof /libtool --mode=execute gprof ./foo
+
 ../../../configure --prefix=${curdir} \
 CFLAGS="-Wall" \
 CXXFLAGS="-Wall" \
