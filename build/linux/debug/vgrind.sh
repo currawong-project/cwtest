b51792ca50d77dc87eb771b0b608f211786b71e7
kevin <kevin@larke.org>
2020-12-29 11:23:57 -0500

    vgrind.sh : Added 'track-origins' flag to vgrind.

--- build/linux/debug/vgrind.sh
+++ build/linux/debug/vgrind.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=~/sdk/libwebsockets/build/out/lib
-valgrind --leak-check=yes --log-file=vg0.txt bin/cwtest ../../../src/cwtest/cfg/main.cfg $1
+valgrind --leak-check=yes --log-file=vg0.txt --track-origins=yes bin/cwtest ../../../src/cwtest/cfg/main.cfg $1
 
