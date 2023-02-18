087f00ac7ddc3bcc03e55d0f46f80a259f8f34bb
kevin <kevin@larke.org>
2023-02-18 13:58:09 -0500

    main.cpp/main.cfg : Added audioDevFileTest().

--- src/cwtest/main.cpp
+++ src/cwtest/main.cpp
@@ -52,6 +52,7 @@
 #endif
 
 #include "cwAudioBuf.h"
+#include "cwAudioDeviceFile.h"
 #include "cwTcpSocket.h"
 #include "cwTcpSocketSrv.h"
 #include "cwTcpSocketTest.h"
@@ -372,6 +373,7 @@ cw::rc_t spscQueueTmpl(        const cw::object_t* cfg, const cw::object_t* args
 cw::rc_t serialPortSrvTest(    const cw::object_t* cfg, const cw::object_t* args, int argc, const char* argv[] ) { return cw::serialPortSrvTest(); }
 cw::rc_t textBufTest(          const cw::object_t* cfg, const cw::object_t* args, int argc, const char* argv[] ) { return cw::textBuf::test(); }
 cw::rc_t audioBufTest(         const cw::object_t* cfg, const cw::object_t* args, int argc, const char* argv[] ) { return cw::audio::buf::test(); }
+cw::rc_t audioDevFileTest(     const cw::object_t* cfg, const cw::object_t* args, int argc, const char* argv[] ) { return cw::audio::device::file::test(args); }
 cw::rc_t mtxTest(              const cw::object_t* cfg, const cw::object_t* args, int argc, const char* argv[] ) { return cw::mtx::test(args); }
 cw::rc_t midiFileTest(         const cw::object_t* cfg, const cw::object_t* args, int argc, const char* argv[] ) { return cw::midi::file::test(args); }
 cw::rc_t audioFileTest(        const cw::object_t* cfg, const cw::object_t* args, int argc, const char* argv[] ) { return cw::audiofile::test(args); }
@@ -745,6 +747,7 @@ int main( int argc, const char* argv[] )
    { "midiDevice", midiDeviceTest },
    { "textBuf", textBufTest },
    { "audioBuf", audioBufTest },
+   { "audioDevFileTest", audioDevFileTest },
    { "audioDevTest",audioDevTest },
    { "audioDevTone", audioDevTestTone },
    { "audioDevAlsa", audioDevAlsaTest },
