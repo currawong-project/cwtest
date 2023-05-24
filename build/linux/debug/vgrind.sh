4c4f956fd69bd3a47c0266d4c682ec6ed2c2a929
kevin <kevin@larke.org>
2023-05-24 15:05:01 -0400

    linux/debug/vgrind.sh : remove LD_LIBRARY_PATH

--- build/linux/debug/vgrind.sh
+++ build/linux/debug/vgrind.sh
@@ -1,3 +1,3 @@
-export LD_LIBRARY_PATH=~/sdk/libwebsockets/build/out/lib
+# export LD_LIBRARY_PATH=~/sdk/libwebsockets/build/out/lib
 valgrind --leak-check=yes --log-file=vg0.txt --track-origins=yes bin/cwtest ../../../src/cwtest/cfg/main.cfg $1
 
