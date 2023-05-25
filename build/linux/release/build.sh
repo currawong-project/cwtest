2eedbf1774971385a899cc45ae2505665d05837e
kevin <kevin@larke.org>
2023-05-25 15:54:29 -0400

    release/build.sh : Updated to use release version of libcm

--- build/linux/release/build.sh
+++ build/linux/release/build.sh
@@ -10,8 +10,8 @@ cd ${curdir}
 ../../../configure --prefix=${curdir} --enable-websock --enable-alsa \
 CFLAGS="-Wall" \
 CXXFLAGS="-Wall" \
-CPPFLAGS="-I${HOME}/src/libcm/build/linux/debug/include" \
-LDFLAGS="-L${HOME}/src/libcm/build/linux/debug/lib" \
+CPPFLAGS="-I${HOME}/src/libcm/build/linux/release/include" \
+LDFLAGS="-L${HOME}/src/libcm/build/linux/release/lib" \
 LIBS=
 
 
